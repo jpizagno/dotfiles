2a4241e875ba3ee89ad5f0da9919007717a3b583
jpizagno <jpizagno@gmail.com>
2019-05-24 22:08:47 +0200

    added config

--- ubuntu/aws.sh
+++ ubuntu/aws.sh
@@ -5,7 +5,7 @@
  cat rootkey.csv 
  export AWS_ACCESS_KEY_ID=A*****A
  export AWS_SECRET_ACCESS_KEY=7***A
- 
+ aws configure
  git config --global credential.helper '!aws codecommit credential-helper $@'
  git config --global credential.UseHttpPath true
  git status
