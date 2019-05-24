0554b9204eae3ff5358b12ddabd7cd9604002884
jpizagno <jpizagno@gmail.com>
2019-05-24 21:58:17 +0200

    added aws.sh

---
+++ ubuntu/aws.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+ cat rootkey.csv 
+ sudo apt install awscli
+ cat rootkey.csv 
+ export AWS_ACCESS_KEY_ID=A*****A
+ export AWS_SECRET_ACCESS_KEY=7***A
+ 
+ git config --global credential.helper '!aws codecommit credential-helper $@'
+ git config --global credential.UseHttpPath true
+ git status
+
+
